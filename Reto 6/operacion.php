f1543420c20cef55ec1ac7412971ab8e05617a05
JuanTrop <jortiza2@unicartagena.edu.co>
2020-10-30 00:16:13 -0500

    Cambios terminados Reto 6

--- Reto 6/operacion.php
+++ Reto 6/operacion.php
@@ -1,11 +1,11 @@
 <?php
 
-require_once 'operacion.php';
+require_once 'realizarOperacion.php';
 
 if(isset($_GET['operador1']) && isset($_GET['operador2']) && isset($_GET['operacion'])) {
    $operador_1 = $_GET['operador1'];
    $operador_2 = $_GET['operador2'];
-   $operacion = $_GET['operaciones'];
+   $operacion = $_GET['operacion'];
 
    echo realizarOperacion($operador_1,$operador_2,$operacion); 
 }
--- Reto 6/realizarOperacion.php
+++ Reto 6/realizarOperacion.php
@@ -2,17 +2,14 @@
 
 function realizarOperacion($operador_1,$operador_2,$operacion) {
     
-    $resultado = 0;
 
-    if($operacion == '+'){
-        $resultado =  $operador_1 + $operador_2;
+    if($operacion == urldecode('%2B')){
+        return ($operador_1 + $operador_2);
     } elseif($operacion == '-'){
-        $resultado = $operador_1 - $operador_2;
+        return $operador_1 - $operador_2;
     } elseif($operacion == '*' || $operacion == 'x'){
-        $resultado = $operador_1 * $operador_2;
-    } else if($operacion == '/' && $operador_2 != 0) {
-        $resultado = $operador_1 / $operador_2;
+        return $operador_1 * $operador_2;
+    } else if($operacion == urlencode('/') && $operador_2 != 0) {
+        echo $operador_1 / $operador_2;
     }
-
-    return $resultado;
 }

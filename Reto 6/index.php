b92abf884c5c39d055247a610251da443df28931
JuanTrop <jortiza2@unicartagena.edu.co>
2020-10-29 22:40:46 -0500

    Reto 6 comenzado

---
+++ Reto 6/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reto 6 PlatziCodingChallenge</title>
+</head>
+<body>
+<form action="operacion.php" method='get'>
+        <label for="operador1">Operador 1: </label>
+        <input type="text" name="operador1" id="operador1_entrada">
+        <br>
+        <label for="operacion">Operacion</label>
+        <input type="text" name="operacion" id="operacion_entradas">
+        <br>
+        <label for="operador2">Operador 2: </label>
+        <input type="text" name="operador2" id="operador2_entradas">
+        <br>
+        <input type="submit" value="Hallar segundos">
+    </form>
+</body>
+</html>
---
+++ Reto 6/operacion.php
@@ -0,0 +1,5 @@
+<?php
+
+if(isset($_GET['horas']) && isset($_GET['minutos'])) {
+    
+}
